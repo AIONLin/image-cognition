dc1792933951609f421ac1e5781a421387b879be
tenkai <skylien0966065599@gmail.com>
2021-07-01 14:57:59 +0800

    test

--- image cognition/MainWindow.xaml.cs
+++ image cognition/MainWindow.xaml.cs
@@ -12,6 +12,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NumSharp;
+using NumSharp.Backends;
+using NumSharp.Backends.Unmanaged;
+using SharpCV;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Tensorflow;
+using static Tensorflow.Binding;
+using static SharpCV.Binding;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+
 
 namespace image_cognition
 {
@@ -23,7 +41,24 @@ namespace image_cognition
         public MainWindow()
         {
             InitializeComponent();
-
+            public bool Run()
+            {
+                string url = "https://github.com/SciSharp/SciSharp-Stack-Examples/blob/master/data/data_CnnInYourOwnData.zip";
+                Directory.CreateDirectory(Name);
+                Utility.Web.Download(url, Name, "data_CnnInYourOwnData.zip");
+                Utility.Compress.UnZip(Name + "\\data_CnnInYourOwnData.zip", Name);
+                BuildGraph();
+​
+                using (var sess = tf.Session())
+                {
+                    Train(sess);
+                    Test(sess);
+                }
+​
+                TestDataOutput();
+​
+                return accuracy_test > 0.98;
+​            }
         }
     }
 }
